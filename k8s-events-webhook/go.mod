221bbdd822d9e41b53807ace3587f63448c00fae
Aleksei Khasanov <akhasano@gmail.com>
2023-03-08 22:37:40 +0300

    Скрипт для доставки событий кубер-кластера в ClickHouse

---
+++ k8s-events-webhook/go.mod
@@ -0,0 +1,7 @@
+module k8s-events
+
+go 1.18
+
+require github.com/roistat/go-clickhouse v1.0.1
+
+require github.com/stretchr/testify v1.8.2 // indirect
---
+++ k8s-events-webhook/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const TableName = "k8s_events.events"
+
+type Event struct {
+	Eventmeta struct {
+		Kind      string `json:"kind"`
+		Name      string `json:"name"`
+		Namespace string `json:"namespace"`
+		Reason    string `json:"reason"`
+	} `json:"eventmeta"`
+	Text string    `json:"text"`
+	Time time.Time `json:"time"`
+}
+
+type DBConn struct {
+	DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASS, CACERT string
+	BATCH                                               int
+	certpool                                            *x509.CertPool
+	conn                                                *http.Client
+	Events                                              []Event
+}
+
+func (dbconn *DBConn) Connect() {
+	log.Println("Подключаюсь к ClickHouse серверу")
+	log.Println("Создаю структуру с корневым сертификатом яндекса")
+	dbconn.certpool = x509.NewCertPool()
+	dbconn.certpool.AppendCertsFromPEM([]byte(dbconn.CACERT))
+	log.Println("Инициализирую http клиента")
+	dbconn.conn = &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: dbconn.certpool,
+			},
+		},
+	}
+}
+
+func (dbconn *DBConn) SetVariables() {
+	log.Println("Считываю переменные окружения")
+	dbconn.DB_HOST = getVariable("DB_HOST", true)
+	dbconn.DB_PORT = getVariable("DB_PORT", true)
+	dbconn.DB_NAME = getVariable("DB_NAME", true)
+	dbconn.DB_USER = getVariable("DB_USER", true)
+	dbconn.DB_PASS = getVariable("DB_PASS", true)
+	dbconn.CACERT = getVariable("CACERT", true)
+	dbconn.BATCH, _ = strconv.Atoi(getVariable("BATCH", true))
+}
+
+func (dbconn *DBConn) handler(w http.ResponseWriter, r *http.Request) {
+	b, _ := io.ReadAll(r.Body)
+	event := Event{}
+	err := json.Unmarshal(b, &event)
+	checkError(err)
+	dbconn.Events = append(dbconn.Events, event)
+	if len(dbconn.Events) >= dbconn.BATCH {
+		log.Println("Отправляем накопленные данные в количестве " + strconv.Itoa(dbconn.BATCH) + " событий в БД и очищаем структуру")
+		dbconn.SendDataToClickHouseDB()
+		dbconn.Events = nil
+	}
+}
+
+func (dbconn *DBConn) PrepareEventsAsString() string {
+	var values string
+	for _, m := range dbconn.Events {
+		values = values + "('" + m.Eventmeta.Kind + "','" + m.Eventmeta.Name + "','" + m.Eventmeta.Namespace + "','" + m.Eventmeta.Reason + "','" + m.Text + "','" + strconv.FormatInt(m.Time.UnixNano(), 10) + "'),"
+	}
+	return values
+}
+
+func (dbconn *DBConn) SendHTTPRequest(m string, q string) string {
+	req, _ := http.NewRequest(m, fmt.Sprintf("https://%s:%s/", dbconn.DB_HOST, dbconn.DB_PORT), nil)
+	query := req.URL.Query()
+	query.Add("database", dbconn.DB_NAME)
+	query.Add("query", q)
+	req.URL.RawQuery = query.Encode()
+	req.Header.Add("X-ClickHouse-User", dbconn.DB_USER)
+	req.Header.Add("X-ClickHouse-Key", dbconn.DB_PASS)
+	resp, err := dbconn.conn.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+	return string(data)
+}
+
+func (dbconn *DBConn) IsTableExist() bool {
+	log.Println("Проверяю существует ли таблица")
+	data := dbconn.SendHTTPRequest("GET", "Exists table "+TableName)
+	ret, err := strconv.Atoi(strings.Trim(string(data), "\n"))
+	checkError(err)
+	if ret == 0 {
+		log.Println("Таблица не существует.")
+		return false
+	} else {
+		log.Println("Таблица существует")
+		return true
+	}
+}
+
+func (dbconn *DBConn) CreateTable() {
+	log.Println("Пробую создать")
+	_ = dbconn.SendHTTPRequest("POST", "CREATE TABLE k8s_events.events (`kind` String, `name` String, `namespace` String, `reason` String, `text` String, `time` Int64) ENGINE = Log;")
+	log.Println("Таблица создана")
+}
+
+func (dbconn *DBConn) SendDataToClickHouseDB() {
+	_ = dbconn.SendHTTPRequest("POST", "INSERT INTO events (kind,name,namespace,reason,text,time) values "+dbconn.PrepareEventsAsString())
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getVariable(curVar string, mandatory bool) string {
+	log.Println("Считываю переменную " + curVar)
+	tmpVar := os.Getenv(curVar)
+	if len(tmpVar) == 0 && mandatory {
+		log.Fatal("Переменная " + curVar + " не задана! Параметр обязательный.")
+	}
+	return tmpVar
+}
+
+func main() {
+	log.Println("Инициализирую структуру")
+	dbconn := DBConn{}
+	dbconn.SetVariables()
+	dbconn.Connect()
+
+	if !dbconn.IsTableExist() {
+		dbconn.CreateTable()
+	}
+	http.HandleFunc("/webhook", dbconn.handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+}
